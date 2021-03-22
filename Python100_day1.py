c3c3cf47a0dcef442b10bba1f4591a629b6fd39e
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-03-22 21:53:58 +0800

    Add files via upload

--- Python100_day1.py
+++ Python100_day1.py
@@ -215,20 +215,203 @@
 # #     a, b = b, a + b
 # #     print(a, end=' ')
 
-#完美数
+# #完美数
+#
+# import math
+#
+# for i in range(1,1000):
+#     result = 0
+#     for num in range(1,int(math.sqrt(i)) + 1):   # 这一步比较难想出来，math.sqrt(i) + 1为啥要用这种？？？
+#         if i % num == 0:
+#             result += num
+#             if num > 1 and i // num != num:
+#                 result += i // num
+#     if result == i:
+#         print(i)
 
-import math
+# #输入M和N计算C(M,N)
+#
+# m = int(input('m = '))
+#
+# n = int(input('n = '))
+#
+# fm = 1
+# for num in range(1,m + 1):
+#     fm *= num
+# fn = 1
+# for num in range(1,n + 1):
+#     fn *= num
+# fm_n = 1
+# for num in range(1,m - n +1):
+#     fm_n *= num
+# print(fm // fn // fm_n)
 
-for i in range(1,1000):
-    result = 0
-    for num in range(1,int(math.sqrt(i)) + 1):   # 这一步比较难想出来，math.sqrt(i) + 1为啥要用这种？？？
-        if i % num == 0:
-            result += num
-            if num > 1 and i // num != num:
-                result += i // num
-    if result == i:
-        print(i)
+#
+# def fac(num):
+#     """求阶乘"""
+#     result = 1
+#     for n in range(1,num + 1):
+#         result *= n
+#     return result
+# m = int(input('m = '))
+# n = int(input('n = '))
+#
+# print(fac(m) // fac(n) // fac(m - n))
 
+
+# from random import randint
+#
+# def roll_dice(n=2):
+#     """摇色子"""
+#     total = 0
+#     for _ in range(n):
+#         total += randint(1,6)
+#     return total
+# def add(a = 0,b = 0,c = 0):
+#     """三个数相加"""
+#     return a + b + c
+#
+# print(roll_dice())
+#
+# print(roll_dice(3))
+# print(add())
+# print(add(1))
+# print(add(1, 2))
+# print(add(1, 2, 3))
+#
+# print(add(c = 50,a = 100,b = 200))
+
+
+# 在参数名前面的*表示args是一个可变参数
+
+# def add(*args):
+#     total = 0
+#     for val in args:
+#         total += val
+#     return total
+
+#在调用add函数时可以传入0个或者多个参数
+
+# print(add())
+# print(add(1))
+# print(add(1,2))
+# print(add(1,2,3))
+# print(add(1,3,5,7,9))
+
+
+# #P7 最大公约数最小公倍数
+#
+# x = int(input('请输入x：'))
+# y = int(input('请输入y：'))
+#
+# def gcd(x,y):
+#     """求最大公约数"""
+#     (x,y) = (y,x) if x > y else (x,y)  #先比较大，找出大小值
+#     for factor in range(x, 0, -1):
+#         if x % factor == 0 and y % factor == 0:
+#             return factor
+# def lcm(x,y):
+#     """求最小公倍数"""
+#     return x * y // gcd(x,y)  #双斜杠表示地板除法，向下取整
+
+
+# #P8 定义回文数，回文数是指1234321之类的，正反读都相同的数字
+#
+# def is_palindrome(num):
+#     """判断一个数是不是回文数"""
+#     temp = num
+#     total = 0
+#     while temp > 0:
+#         total = total * 10 + temp % 10 #temp除10取余数作为个位数，作为新的total，下一次乘10升为十位数
+#         temp //= 10  #temp除10向下取整可以留下依此取出十位、百位、千位等等
+#     return total == num
+#
+#
+#
+# #P9 判断一个数字是不是素数,质数是指在大于1的自然数中，除了1和它本身以外不再有其他因数的自然数
+# def is_prime(num):
+#     """判断一个数是不是素数"""
+#     for factor in range(2,int(num ** 0.5) + 1):
+#         if num % factor == 0:
+#             return False
+#     return True if num != 1 else False
+#
+#
+# #P10 写一个程序判断输入的正整数是不是回文素数，上述两个模块是函数体，下面的部分是真正的给参数，调用函数的部分
+#
+# if __name__ == '__main__':
+#     num = int(input('请输入正整数：'))
+#     if is_palindrome(num) and is_prime(num):
+#         print('%d是回文素数'%num)
+
+
+# s1 = '\'hello, world!\''
+# s2 = '\n\\hello, world!\\\n'
+# print(s1, s2, end='')
+
+# s1 = r'\'hello, world!\''
+# s2 = r'\n\\hello, world!\\\n'
+# print(s1, s2, end='')
+
+
+# #example
+#
+# s1 = 'hello ' * 3
+# print(s1)
+# s2 = 'world'
+# s1 += s2
+# print(s1)
+# print(s2)
+#
+# print('ll' in s1)
+# str2 = 'abc123456'
+#
+# print(str2[2]) #从0开始正数第二个元素
+#
+# print(str2[2:5]) #从0开始第二个到第五个元素，不包含第五个
+# print(str2[2:])  #从第二个元素开始到结尾
+# print(str2[2::2])  #从第二个开始到结尾每次步长为2
+# print(str2[::2])   #从开始到结尾，步长为2
+# print(str2[::-1])  #从结尾到开始，步长为1
+# print(str2[-3:-1]) #从结尾开始到倒数第三个，不包含倒数第一个
+
+
+#example
+
+str1 = 'hello, world!'
+print(len(str1))  #通过内置函数len计算字符串长度
+
+print(str1.capitalize())  #将字符串首字母大写后输出
+
+print(str1.title())    #将字符串的每个单词首字母大写之后输出
+
+print(str1.upper())    #将字符串变成大写之后输出
+
+print(str1.find('or')) #查找子串在字符串中的位置
+print(str1.find('shit'))  #找不到不会引发异常
+
+print(str1.index('or')) #与find作用类似，但找不到会引发异常
+#print(str1.index('shit'))  #找不到，引发异常
+
+print(str1.startswith('He'))  #检查字符串是否以指定的字符开始，返回值为布尔类型
+print(str1.startswith('hel'))
+
+print(str1.endswith('!'))    #检查字符串是否以指定的字符结尾
+
+print(str1.center(50,'*'))   #将字符串以指定的宽度剧中并在两侧填充指定字符
+print(str1.rjust(50,' '))    #将字符串以指定的宽度靠右放置并在左侧填充指定字符
+
+str2 = 'abc123456'
+# 检查字符串是否由数字构成
+print(str2.isdigit())  # False
+# 检查字符串是否以字母构成
+print(str2.isalpha())  # False
+# 检查字符串是否以数字和字母构成
+print(str2.isalnum())  # True
+str3 = '  jackfrued@126.com '
+print(str3)
+# 获得字符串修剪左右两侧空格之后的拷贝
+print(str3.strip())
 
 
 
