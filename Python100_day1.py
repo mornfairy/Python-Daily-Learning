2a4a50f6751857a4a91d7dd451f89369bcec18d1
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-04-06 21:50:13 +0800

    Add files via upload

--- Python100_day1.py
+++ Python100_day1.py
@@ -1248,133 +1248,47 @@
 
 
 
-#将字典或者列表以JSON格式保存到文件中
-
-import json
-
-def main():
-    mydict = {
-        'name': '文豪',
-        'age' : 27,
-        'phone' : 18539068687,
-        'friends' : ['王大锤', '白元芳'],
-        'cars' : [
-            {'brand': 'Audi', 'max_speed' : 280},
-            {'brand': 'Benz', 'max_speed' : 320},
-        ]
-    }
-    try:
-        with open('data.json', 'w',encoding = 'utf-8') as fs:
-            json.dump(mydict,fs)
-    except IOError as e:
-        print(e)
-    print('保存数据完成!')
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# #将字典或者列表以JSON格式保存到文件中
+#
+# import json
+#
+# def main():
+#     mydict = {
+#         'name': '文豪',
+#         'age' : 27,
+#         'phone' : 18539068687,
+#         'friends' : ['王大锤', '白元芳'],
+#         'cars' : [
+#             {'brand': 'Audi', 'max_speed' : 280},
+#             {'brand': 'Benz', 'max_speed' : 320},
+#         ]
+#     }
+#     try:
+#         with open('data.json', 'w',encoding = 'utf-8') as fs:
+#             json.dump(mydict,fs)
+#     except IOError as e:
+#         print(e)
+#     print('保存数据完成!')
+#
+# if __name__ == '__main__':
+#     main()
+
+
+# # 求平方函数
+#
+# squares = []
+# for value in range(1,11):
+#     square = value ** 2
+#     squares.append(square)
+#
+#     print(squares)
+
+# #更简洁的方法
+#
+# squares = []
+# for value in range(1,11):
+#     squares.append(value ** 2)
+#     print(squares)
 
 
 
@@ -1384,9 +1298,9 @@ if __name__ == '__main__':
 # import turtle
 #
 #
-# def draw_rectangle(x,y,width,height):
+# def draw_rectangle(x, y, width, height):
 #     """"绘制矩形"""
-#     turtle.goto(x,y)
+#     turtle.goto(x, y)
 #     turtle.pencolor('red')
 #     turtle.fillcolor('red')
 #     turtle.begin_fill()
@@ -1395,8 +1309,295 @@ if __name__ == '__main__':
 #         turtle.left(90)
 #         turtle.forward(height)
 #         turtle.left(90)
-#         turtle.end_fill()
+#     turtle.end_fill()
 #
-# def draw_star(x,y,radius):
+# def draw_star(x, y, radius):
 #     """"绘制五角星"""
-#     turtle.setpos(x,y)
+#     turtle.setpos(x, y)
+#     pos1 = turtle.pos()
+#     turtle.circle(-radius, 72)
+#     pos2 = turtle.pos()
+#     turtle.circle(-radius, 72)
+#     pos3 = turtle.pos()
+#     turtle.circle(-radius, 72)
+#     pos4 = turtle.pos()
+#     turtle.circle(-radius, 72)
+#     pos5 = turtle.pos()
+#     turtle.color('yellow', 'yellow')
+#     turtle.begin_fill()
+#     turtle.goto(pos3)
+#     turtle.goto(pos1)
+#     turtle.goto(pos4)
+#     turtle.goto(pos2)
+#     turtle.goto(pos5)
+#     turtle.end_fill()
+#
+#
+# def main():
+#     """主程序"""
+#     turtle.speed(12)
+#     turtle.penup()
+#     x,y = -270, -180
+#
+#     #画国旗主体
+#     width, height = 540, 360
+#     draw_rectangle(x, y, width, height)
+#
+#     #画大星星
+#     pice = 22
+#     center_x, center_y = x + 5 * pice, y + height - pice * 5
+#     turtle.goto(center_x, center_y)
+#     turtle.left(90)
+#     turtle.forward(pice * 3)
+#     turtle.right(90)
+#     draw_star(turtle.xcor(), turtle.ycor(), pice * 3)
+#     x_poses, y_poses = [10, 12, 12, 10], [2, 4, 7, 9]
+#
+#     #画小星星
+#     for x_pos, y_pos in zip(x_poses, y_poses):
+#         turtle.goto(x + x_pos * pice, y + height - y_pos * pice)
+#         turtle.left(turtle.towards(center_x, center_y) - turtle.heading())
+#         turtle.forward(pice)
+#         turtle.right(90)
+#         draw_star(turtle.xcor(), turtle.ycor(), pice)
+#
+#     #隐藏海龟
+#     turtle.ht()
+#
+#     #显示绘画窗口
+#     turtle.mainloop()
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+
+
+# #绘制小猪佩奇
+#
+# """
+# 绘制小猪佩奇
+# """
+# from turtle import *
+#
+#
+# def nose(x,y):
+#     """画鼻子"""
+#     penup()
+#     # 将海龟移动到指定的坐标
+#     goto(x,y)
+#     pendown()
+#     # 设置海龟的方向（0-东、90-北、180-西、270-南）
+#     setheading(-30)
+#     begin_fill()
+#     a = 0.4
+#     for i in range(120):
+#         if 0 <= i < 30 or 60 <= i <90:
+#             a = a + 0.08
+#             # 向左转3度
+#             left(3)
+#             # 向前走
+#             forward(a)
+#         else:
+#             a = a - 0.08
+#             left(3)
+#             forward(a)
+#     end_fill()
+#     penup()
+#     setheading(90)
+#     forward(25)
+#     setheading(0)
+#     forward(10)
+#     pendown()
+#     # 设置画笔的颜色(红, 绿, 蓝)
+#     pencolor(255, 155, 192)
+#     setheading(10)
+#     begin_fill()
+#     circle(5)
+#     color(160, 82, 45)
+#     end_fill()
+#     penup()
+#     setheading(0)
+#     forward(20)
+#     pendown()
+#     pencolor(255, 155, 192)
+#     setheading(10)
+#     begin_fill()
+#     circle(5)
+#     color(160, 82, 45)
+#     end_fill()
+#
+#
+# def head(x, y):
+#     """画头"""
+#     color((255, 155, 192), "pink")
+#     penup()
+#     goto(x,y)
+#     setheading(0)
+#     pendown()
+#     begin_fill()
+#     setheading(180)
+#     circle(300, -30)
+#     circle(100, -60)
+#     circle(80, -100)
+#     circle(150, -20)
+#     circle(60, -95)
+#     setheading(161)
+#     circle(-300, 15)
+#     penup()
+#     goto(-100, 100)
+#     pendown()
+#     setheading(-30)
+#     a = 0.4
+#     for i in range(60):
+#         if 0<= i < 30 or 60 <= i < 90:
+#             a = a + 0.08
+#             lt(3) #向左转3度
+#             fd(a) #向前走a的步长
+#         else:
+#             a = a - 0.08
+#             lt(3)
+#             fd(a)
+#     end_fill()
+#
+#
+# def ears(x,y):
+#     """画耳朵"""
+#     color((255, 155, 192), "pink")
+#     penup()
+#     goto(x, y)
+#     pendown()
+#     begin_fill()
+#     setheading(100)
+#     circle(-50, 50)
+#     circle(-10, 120)
+#     circle(-50, 54)
+#     end_fill()
+#     penup()
+#     setheading(90)
+#     forward(-12)
+#     setheading(0)
+#     forward(30)
+#     pendown()
+#     begin_fill()
+#     setheading(100)
+#     circle(-50, 50)
+#     circle(-10, 120)
+#     circle(-50, 56)
+#     end_fill()
+#
+#
+# def eyes(x,y):
+#     """画眼睛"""
+#     color((255, 155, 192), "white")
+#     penup()
+#     setheading(90)
+#     forward(-20)
+#     setheading(0)
+#     forward(-95)
+#     pendown()
+#     begin_fill()
+#     circle(15)
+#     end_fill()
+#     color("black")
+#     penup()
+#     setheading(90)
+#     forward(12)
+#     setheading(0)
+#     forward(-3)
+#     pendown()
+#     begin_fill()
+#     circle(3)
+#     end_fill()
+#     color((255, 155, 192), "white")
+#     penup()
+#     seth(90)
+#     forward(-25)
+#     seth(0)
+#     forward(40)
+#     pendown()
+#     begin_fill()
+#     circle(15)
+#     end_fill()
+#     color("black")
+#     penup()
+#     setheading(90)
+#     forward(12)
+#     setheading(0)
+#     forward(-3)
+#     pendown()
+#     begin_fill()
+#     circle(3)
+#     end_fill()
+#
+#
+# def cheek(x,y):
+#     """画脸颊"""
+#     color((255, 155, 192))
+#     penup()
+#     goto(x,y)
+#     pendown()
+#     setheading(0)
+#     begin_fill()
+#     circle(30)
+#     end_fill()
+#
+#
+# def mouth(x,y):
+#     """画嘴巴"""
+#     color(239, 69, 19)
+#     penup()
+#     goto(x, y)
+#     pendown()
+#     setheading(-80)
+#     circle(30, 40)
+#     circle(40, 80)
+#
+#
+# def setting():
+#     """设置参数"""
+#     pensize(4)
+#     # 隐藏海龟
+#     hideturtle()
+#     colormode(255)
+#     color((255, 155, 192), "pink")
+#     setup(840, 500)
+#     speed(10)
+#
+#
+# def main():
+#     """主函数"""
+#     setting()
+#     nose(-100, 100)
+#     head(-69, 167)
+#     ears(0, 160)
+#     eyes(0, 140)
+#     cheek(80, 10)
+#     mouth(-20, 30)
+#     done()
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+# #华氏度转摄氏度
+#
+# f = float(input('请输入华氏温度：'))
+# c = (f - 32) / 1.8
+# print('%.1f华氏度 = %.1f摄氏度' % (f, c))
+
+#给半径，计算圆的面积和周长
+
+# r = float(input('请输入半径:'))
+# pi = 3.1415926
+# s = pi * r * r
+# c = 2 * pi * r
+# print('面积为:%.1f\n周长为:%.1f' % (s, c))
+
+# import math
+#
+# radius = float(input('请输入圆的半径:'))
+# perimeter = 2 * math.pi * radius
+# area = math.pi * radius * radius
+# print('周长：%.2f' % perimeter)
+# print('面积：%.2f' % area)
