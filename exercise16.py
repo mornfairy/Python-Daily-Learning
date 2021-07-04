38a678fa08e3f31667f102f0e383bb27dcff3517
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-07-04 16:08:39 +0800

    Add files via upload

---
+++ exercise16.py
@@ -0,0 +1,13 @@
+#  题目：输出指定格式的日期。
+
+import time
+import datetime
+
+print(time.time())
+print(time.localtime())
+print(time.asctime())
+print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
+
+print(datetime.date.today())
+print(datetime.date.today().strftime('%d/%m/%Y'))
+print(datetime.date(1994, 12, 24))
