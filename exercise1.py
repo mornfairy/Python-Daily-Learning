a478f57a170bde06c2a6efa1f69c46ea255aa237
wenhao <69474330+Sirwenhao@users.noreply.github.com>
2021-07-06 16:57:35 +0800

    Delete exercise1.py

--- exercise1.py
+++
@@ -1,6 +0,0 @@
-#  有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
-for i in range(1, 5):
-    for j in range(1, 5):
-        for k in range(1, 5):
-            if(i != j) and (j != k ) and (i != k): # 没有想到这一步，只想到了个位十位百位
-                print(i,j,k)
